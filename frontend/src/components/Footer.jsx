50481fd12c2bfe2606945e8cdf00ad14170ff00b
CHAITHANYA4523 <devichaithanya45@gmail.com>
2025-02-18 18:24:52 +0530

    refactor Footer component layout and content for improved structure and clarity

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from 'react';
+import { assets } from '../assets/assets';
 
 const Footer = () => {
   return (
     <div>
-      <div className='flex flex-col sm:grid grid-cols-[3xl_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+      <div className='flex flex-row gap-14 my-10 mt-40 text-sm'>
+        <div className='w-1/2'>
+          <img src={assets.logo} alt="" className='mb-5 w-32'/>
+          <p className='text-gray-600'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias, laborum?
+          </p>
+        </div>
+        <div className='w-1/2'>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
         <div>
-            <img src={assets.logo} alt="" className='mb-5 w-32'/>
-            <p className='w-full md:w-2/3 text-gray-600'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            </p>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+1-244-424-784</li>
+            <li>contact@foreveru.com</li>
+          </ul>
         </div>
       </div>
-    </div>
-  )
-}
 
-export default Footer
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025@forever.com All rights reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
