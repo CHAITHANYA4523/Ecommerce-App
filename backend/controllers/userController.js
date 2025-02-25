3c6895bf3cb29c38260f197638b97088ee707496
CHAITHANYA4523 <devichaithanya45@gmail.com>
2025-02-26 00:50:48 +0530

    add login functionality with error handling and token generation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,8 +9,29 @@ const createToken = (id) => {
 
 //Route for user Login
 const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await userModel.findOne({email});
 
+        if(!user){
+            return res.json({success: false, message: "User does not exists"});
+        }
+
+        const isMatched = await bcrypt.compare(password, user.password);
+
+        if(isMatched){
+            const token = createToken(user._id);
+            res.json({success: true, token});
+        }
+        else{
+            res.json({success: false, message: "invalid credentials"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
+
 //Route for user register
 const registerUser = async (req, res) => {
     try{
